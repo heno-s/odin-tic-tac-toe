6bd03b1d611dc1dc7e2225338944abe2263f23e1
janko12 <henrich.sury97@gmail.com>
2023-02-01 14:01:49 +0100

    Init game through js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+const form = document.forms[0];
+const main = document.querySelector("main");
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const player1 = {
+        name: form.player1Name.value,
+        symbol: form.player1Symbol.value,
+        id: "1",
+    };
+    const player2 = {
+        name: form.player2Name.value,
+        symbol: form.player2Symbol.value,
+        id: "2",
+    };
+    game.init(player1, player2);
+
+    form.classList.add("hide");
+    main.classList.remove("hide");
+});
+
 const gameboard = (function () {
     const gameboard = [];
     for (let i = 0; i < 9; i++) {
@@ -37,7 +58,44 @@ const displayController = (function () {
         scoreDOM.textContent = score;
     }
 
-    return { placeSymbol, updateScore };
+    function init(player1, player2) {
+        const scoreboard1 = document.querySelector("#p1");
+        const scoreboard2 = document.querySelector("#p2");
+
+        scoreboard1.querySelector(".name").textContent = player1.name;
+        scoreboard1.querySelector("[data-score]").textContent = "0";
+        scoreboard1.querySelector("[data-symbol]").textContent =
+            player1.symbol;
+        scoreboard2.querySelector(".name").textContent = player2.name;
+        scoreboard2.querySelector("[data-score]").textContent = "0";
+        scoreboard2.querySelector("[data-symbol]").textContent =
+            player2.symbol;
+    }
+
+    return { placeSymbol, updateScore, init };
+})();
+
+const game = (function () {
+    let _player1;
+    let _player2;
+    let _turningPlayer;
+
+    function init(player1, player2) {
+        _player1 = playerFactory(
+            player1.name,
+            player1.symbol,
+            player1.id
+        );
+        _player2 = playerFactory(
+            player2.name,
+            player2.symbol,
+            player2.id
+        );
+        _turningPlayer = Math.random() > 0.5 ? _player1 : _player2;
+
+        displayController.init(player1, player2);
+    }
+    return { init };
 })();
 
 function playerFactory(name, symbol, id) {
